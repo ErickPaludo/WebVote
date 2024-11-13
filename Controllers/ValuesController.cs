37b58ec5c90c791ecab87d44a43025b8b2eac9ae
Érick Augusto Paludo <erickpaludoyt@gmail.com>
2024-11-12 22:15:31 -0300

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebVote.Entidades;
+
+namespace WebVote.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet(Name = "Apuracao")]
+        public IActionResult Get()
+        {
+            var candidatos = new Candidatos
+            {
+                candidatos = new List<People>  // Inicializa a lista de `People`
+        {
+            new People { nome = "Mario", votos = 12 }
+        }
+            };
+            return Ok(candidatos);
+        }
+    }
+}
---
+++ Entidades/CandidatosJson.cs
@@ -0,0 +1,11 @@
+﻿namespace WebVote.Entidades;
+
+public class Candidatos
+{
+    public List<People> candidatos { get; set; }
+}
+public class People
+{
+    public string? nome { get; set; }
+    public int votos { get; set; }
+}
